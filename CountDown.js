939b8f9933d223d7e123cec1d5bfb9b94f098142
veronika123 <melilla@centrum.sk>
2013-03-13 09:36:38 +0100

    Update CountDown.js

--- CountDown.js
+++ CountDown.js
@@ -1,44 +1,73 @@
-﻿function Countdown(cd) {
+function Countdown(endTime) {
  
- 	this.cd = cd;
+
  	
- 	function setElement(id, value) {
-		if (value.length < 2) {
-		value = "0" + value;
+ 	function setElement(id, fDays, fHours, fMinutes, fSeconds) {
+ 		// Ak je dlzka hodnoty mensia ako 2, zmen ju. (priklad: 9 sekund zmen na 09 sekund
+		if (fDays < 10) {
+			fDays = "0" + fDays;
+		}
+		if (fHours < 10) {
+			fHours = "0" + fHours;
+		}
+		if (fMinutes < 10) {
+			fMinutes = "0" + fMinutes;
 		}
 
-		if (id == "countdown-days" && "countdown-hours" && "countdown-minutes" && "countdown-seconds" && value == "0") {
-		window.location = "PracticalTest2.html";
+		if (fSeconds < 10) {
+			fSeconds = "0" + fSeconds;
 		}
-		
-		window.document.getElementById(id).innerHTML = value;
-		}
+		if (fDays == "01") {
+			fDaysKoncovka = " ";
+		} else {
+			fDaysKoncovka = "s"
+		};
+
+		if (fHours == "01") {
+			fHoursKoncovka = " ";
+		} else {
+			fHoursKoncovka = "s"
+		};
+
+		if (fMinutes == "01") {
+			fMinutesKoncovka = " ";
+		} else {
+			fMinutesKoncovka = "s"
+		};
+
+		if (fSeconds == "01") {
+			fSecondsKoncovka = " ";
+		} else {
+			fSecondsKoncovka = "s"
+		};
+		$(id).html("<div class='on'><h1>Countdown untill kick-off</h1><span id='countdown-days'>"+fDays+"<p>day"+fDaysKoncovka+"</p></span><span id='countdown-hours'>"+fHours+"<p>hour"+fHoursKoncovka+"</p></span><span id='countdown-minutes'>"+fMinutes+"<p>minute"+fMinutesKoncovka+"</p></span><span id='countdown-seconds'>"+fSeconds+"<p>second"+fSecondsKoncovka+"</p></span></div>");
+	}
  	
  	function countdown() {
  		now		= new Date();
- 	  	diff	= new Date(this.cd - now);
+ 	  	diff	= new Date(endTime - now);
  	  	
- 		seconds_left  = Math.floor(diff.valueOf() / 1000);
+ 		seconds_left  = Math.floor(diff / 1000);
  	
  		seconds  = Math.floor(seconds_left / 1) % 60;
  		minutes  = Math.floor(seconds_left / 60) % 60;
  		hours    = Math.floor(seconds_left / 3600) % 24;
  		days     = Math.floor(seconds_left / 86400) % 86400;
  		
- 		setElement('countdown-days', days);
- 		setElement('countdown-hours', hours);
- 		setElement('countdown-minutes', minutes);
- 		setElement('countdown-seconds', seconds);
+ 		setElement('.countDown', days, hours, minutes, seconds);
  		
- 		countdown.timer = setTimeout(countdown, 1000);
+
+ 		
+ 		
+ 		timer = setTimeout(countdown, 1000);
  	}
  	
  		
  	function start() {
- 		this.timer = setTimeout(countdown, 1000);
+ 		timer = setTimeout(countdown, 1000);
  	}
  	
- 	start(cd);	
+ 	start(endTime);	
  }
  
- Countdown(new Date("Sep 12 2013 15:00:00"));
+ Countdown(new Date("Sep 12 2013 15:48:00"));
