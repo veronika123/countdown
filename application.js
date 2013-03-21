94129787f85ba3cab34f7277f4888b28be9216f4
veronika123 <melilla@centrum.sk>
2013-03-21 11:44:35 +0100

    Update application.js

--- application.js
+++ application.js
@@ -2,4 +2,9 @@ var options1 = {
   endTime: new Date().getTime()+5000,
 }
 
-$('#count').countdown('init', options1);
+var options2 = {
+	endTime: new Date().getTime()+10000,
+}
+
+$('#count1').countdown('init', options1);
+$ ('#count2').countdown('init', options2);
