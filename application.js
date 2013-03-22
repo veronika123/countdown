379fb3dff1222db92b401073de2b2c258fb07b2d
veronika123 <melilla@centrum.sk>
2013-03-22 13:10:57 +0100

    Update application.js

--- application.js
+++ application.js
@@ -1,10 +1,24 @@
 var options1 = {
-  endTime: new Date().getTime()+5000,
-}
+	endTime: new Date().getTime()+5000,
+	onCountDownEnd: function()
+	{  
+      $("#fixtureList").toggleClass("on off");
+      $("#count1").toggleClass("on off");
+      $("#commentary").toggleClass("on off");
+      $("#heading").toggleClass("on off");
+    }, 
+    onCountDownChange: function() 
+    {
+    	console.log("Zmena.");
+    }
+};
 
 var options2 = {
 	endTime: new Date().getTime()+10000,
-}
+	onCountDownEnd: function(){
+		$("#count2").toggleClass("on off");
+	}
+};
 
 $('#count1').countdown('init', options1);
-$ ('#count2').countdown('init', options2);
+$('#count2').countdown('init', options2);
